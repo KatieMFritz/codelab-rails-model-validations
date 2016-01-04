27d93c649a8d21d0c6309f430fc69fa16b8d38c1
Katie Fritz <katie@katiemfritz.com>
2016-01-03 21:55:27 -0500

    added front-end validation to ratings (number_field)

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,10 @@ class Book < ActiveRecord::Base
   validates :title, uniqueness: {
     message: "cannot be the same as an existing book title"
   }
+  validates :rating, numericality: {
+    only_integer: true,
+    greater_than: 0,
+    less_than_or_equal_to: 5,
+    message: "must be an integer between 1 and 5"
+}
 end
